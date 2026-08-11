7df79b97b8df08b5a0a02a2a7e31c1a0629bcd91
Sergio Pujol Serrano <spujol-s@car15s1.42barcelona.com>
2026-08-11 16:49:53 +0200

    feat(entities): implement player movement and ghost AI FSM

--- src/entities/collectibles.py
+++ src/entities/collectibles.py
@@ -2,7 +2,7 @@ from src.entities.entity import Collectible
 
 
 class Pacgum(Collectible):
-    def __init__(self, cell: tuple[int, int], points: int):
+    def __init__(self, cell: tuple[int, int], points: int) -> None:
         super().__init__(cell, "pacgum", points)
 
 
