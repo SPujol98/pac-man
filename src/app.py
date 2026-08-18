ca374297e009dd32a467012e0068f81e93818dd4
Sabrina Eliana Sanchez Pirotta <sasanche@cbr1s5.42barcelona.com>
2026-08-18 18:26:51 +0200

    file_validator

--- src/app.py
+++ src/app.py
@@ -59,8 +59,9 @@ class App:
         self.maze_data = load_maze(width=width, height=height, seed=seed)
         self.state = GameState.MENU
         self.is_running = True
-        self.highscore_file: str = ""
-        self._highscore_file(self.config.get("highscore_filename"))
+        self.highscore_file: str = self.config.get(
+            "highscore_filename", "highscores.json"
+        )
 
         self.screens: dict[GameState, BaseScreen] = {
             GameState.MENU: MainMenu(self.VIRTUAL_WIDTH, self.VIRTUAL_HEIGHT),
@@ -81,15 +82,6 @@ class App:
                                      self.VIRTUAL_HEIGHT,
                                      self.highscore_file),
         }
-
-    def _highscore_file(self, highscore_file: str) -> None:
-        if "." in highscore_file:
-            if not highscore_file.endswith(".json"):
-                self.highscore_file = "highscores.json"
-                print("[ERROR] The highscore file was corrupted")
-            self.highscore_file = highscore_file
-        else:
-            self.highscore_file = highscore_file + ".json"
 
     def run(self) -> None:
         """Executes the core application loop until execution is terminated.
--- src/systems/config_parser.py
+++ src/systems/config_parser.py
@@ -9,6 +9,7 @@ from pydantic import (
     Field,
     ValidationError,
     model_validator,
+    field_validator
 )
 
 
@@ -59,6 +60,37 @@ class GameConfig(BaseModel):
                 print(f"[Warning] Missing key '{key}'. "
                       "A safe default will be applied.")
         return data
+
+    @field_validator("highscore_filename")
+    @classmethod
+    def validate_no_path_in_highscore(cls, v: str) -> str:
+        path = Path(v)
+        if "/" in v or "\\" in v or path.parent != Path("."):
+            print(f"[Warning] 'highscore_filename' cannot be a path ({v}). "
+                  "Defaulting to 'highscores.json'.")
+            return "highscores.json"
+        return v
+
+    @field_validator("highscore_filename")
+    @classmethod
+    def validate_highscore_filename(cls, v: str) -> str:
+        if "/" in v or "\\" in v or Path(v).parent != Path("."):
+            print(
+                f"[Warning] Invalid path in highscore filename '{v}'. "
+                "Defaulting to 'highscores.json'."
+            )
+            return "highscores.json"
+
+        if "." in v:
+            if not v.endswith(".json"):
+                print(
+                    "[ERROR] The highscore file was corrupted or"
+                    "invalid format. Defaulting to 'highscores.json'."
+                )
+                return "highscores.json"
+            return v
+
+        return f"{v}.json"
 
 
 def strip_comments(text: str) -> str:
